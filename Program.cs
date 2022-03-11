701edab6bf4acb6ae5a0ec4a3f20197d6da1c5b7
Devil Queen <rominafp>
2022-03-11 14:22:27 +0330

    tamrin2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double sum=0;
+int counter=-1;
+int number;
+
+
+
+do
+{
+    
+    
+        System.Console.WriteLine("nomarat ra vared konid");
+        number=Convert.ToInt32(Console.ReadLine());
+        sum=sum+number;
+        counter++;
+        
+    
+    
+    
+} while (number !=0);
+
+System.Console.WriteLine(sum/counter);
